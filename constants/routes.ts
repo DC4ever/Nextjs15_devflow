e22dff985c02d4d354ff00f26199f6c3db32c14b
Jerry Office <birthday228m@gmail.com>
2025-01-05 15:06:31 +0800

    Left & Right Sidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,12 +2,14 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
-  COMUNITY: "/community",
-  COLLECTION: "/collection",
-  JOBS: "/jobs",
-  TAGS: "/tags",
-  PROFILE: "/profile",
   ASK_QUESTION: "/ask-question",
+  COLLECTION: "/collection",
+  COMMUNITY: "/community",
+  JOBS: "/jobs",
+  PROFILE: (id: string) => `/profile/${id}`,
+  QUESTION: (id: string) => `/questions/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
+  SIGN_IN_WITH_OAUTH: `signin-with-oauth`,
 };
 
 export default ROUTES;
